0257496d8bc06713ed62372a55847e17157372d4
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-05-16 13:59:55 +0800

    Add schema in the convex folder

---
+++ convex/schema/index.ts
@@ -0,0 +1,17 @@
+import { defineSchema, defineTable } from 'convex/server';
+import { v } from 'convex/values';
+
+export default defineSchema({
+  document: defineTable({
+    user_id: v.string(),
+    title: v.string(),
+    icon: v.optional(v.string()),
+    cover_image: v.optional(v.string()),
+    content: v.optional(v.string()),
+    parent_document: v.optional(v.id('documents')),
+    is_published: v.boolean(),
+    is_archived: v.boolean(),
+  })
+    .index('by_user', ['user_id'])
+    .index('by_user_parent', ['user_id', 'parent_document']),
+});
