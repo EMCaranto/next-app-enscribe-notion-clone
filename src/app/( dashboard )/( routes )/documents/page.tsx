0aa2c94f6e0d0d2330885054d23f05f20a051959
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-05-16 14:38:03 +0800

    Implement create document convex function in the documents page

--- src/app/( dashboard )/( routes )/documents/page.tsx
+++ src/app/( dashboard )/( routes )/documents/page.tsx
@@ -2,17 +2,38 @@
 
 import React from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 import { useUser } from '@clerk/clerk-react';
+import { useMutation } from 'convex/react';
 import { PlusCircleIcon } from 'lucide-react';
+import { toast } from 'sonner';
 
 import { Button } from '@/components/ui/button';
 
 import ImageOneDark from '@/assets/images/empty-dark-mode.png';
 import ImageOneLight from '@/assets/images/empty-light-mode.png';
 
+import { api } from '../../../../../convex/_generated/api';
+
 export default function DocumentsPage() {
   const { user } = useUser();
+
+  const router = useRouter();
+
+  const createDoc = useMutation(api.documents.index.createDocument);
+
+  const onCreateDocHandler = () => {
+    const promise = createDoc({ title: 'Untitled' }).then((documentId) =>
+      router.push(`/documents/${documentId}`)
+    );
+
+    toast.promise(promise, {
+      loading: 'Creating new note',
+      success: 'New note created',
+      error: 'Failed to create new note',
+    });
+  };
 
   return (
     <div className="flex h-full flex-col items-center justify-center space-y-4">
@@ -31,7 +52,7 @@ export default function DocumentsPage() {
       <p className="text-base font-medium md:text-lg">
         Welcome to {user?.firstName}&apos;s Scribespace
       </p>
-      <Button className="gap-x-2">
+      <Button className="gap-x-2" onClick={onCreateDocHandler}>
         <PlusCircleIcon size={16} />
         <span>Create a note</span>
       </Button>
