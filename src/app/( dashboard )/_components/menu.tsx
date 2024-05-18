7b7ca8ef48f2a10f464ebbc7b183434c7661ba00
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-05-18 15:37:17 +0800

    Add menu component

---
+++ src/app/( dashboard )/_components/menu.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+import { useUser } from '@clerk/clerk-react';
+import { useMutation } from 'convex/react';
+import { MoreHorizontalIcon, Trash2Icon } from 'lucide-react';
+import { toast } from 'sonner';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Skeleton } from '@/components/ui/skeleton';
+
+import { api } from '../../../../convex/_generated/api';
+import { Id } from '../../../../convex/_generated/dataModel';
+
+interface MenuProps {
+  documentId: Id<'documents'>;
+}
+
+export const Menu = ({ documentId }: MenuProps) => {
+  const { user } = useUser();
+
+  const router = useRouter();
+
+  const archiveDocument = useMutation(api.documents.onArchiveDocument);
+
+  const onArchivedHandler = () => {
+    const promise = archiveDocument({ id: documentId });
+
+    toast.promise(promise, {
+      loading: 'Archiving note...',
+      success: 'Note has been archived!',
+      error: 'Failed to archive note.',
+    });
+
+    router.push('/documents');
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={'ghost'} size={'sm'}>
+          <MoreHorizontalIcon className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent onClick={onArchivedHandler}>
+        <DropdownMenuItem>
+          <Trash2Icon className="mr-2 h-4 w-4" />
+          Delete
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <div className="p-2 text-xs text-muted-foreground">
+          Last edited by: {user?.fullName}
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+Menu.Skeleton = function MenuSkeleton() {
+  return <Skeleton className="h-6 w-6" />;
+};
