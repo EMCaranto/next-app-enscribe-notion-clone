946dc4518387a8e08b415a697a9d5df95296ba59
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-05-20 18:47:32 +0800

    Refactor the error page in the app

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -6,26 +6,32 @@ import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 
-import ImageOneDark from '@/assets/images/error-dark-mode.png';
-import ImageOneLight from '@/assets/images/error-light-mode.png';
+import img_one_dark from '@/assets/images/error-dark-mode.png';
+import img_one_light from '@/assets/images/error-light-mode.png';
 
 export default function ErrorPage() {
   return (
-    <div className="flex h-full flex-col items-center justify-center space-y-4">
-      <Image
-        className="block h-[300px] w-[300px] dark:hidden"
-        src={ImageOneDark}
-        alt="img-one-dark"
-      />
-      <Image
-        className="hidden h-[300px] w-[300px] dark:block"
-        src={ImageOneLight}
-        alt="img-one-light"
-      />
-      <h2 className="text-xl font-medium">Something went wrong!</h2>
-      <Button asChild>
-        <Link href={'/documents'}>Go Back</Link>
-      </Button>
-    </div>
+    <main className="h-full w-full">
+      <div className="flex h-full flex-col items-center justify-center space-y-4">
+        <Image
+          className="hidden w-[300px] dark:block md:w-[500px]"
+          src={img_one_dark}
+          alt="img-one-dark"
+          priority
+        />
+        <Image
+          className="block w-[300px] dark:hidden md:w-[500px]"
+          src={img_one_light}
+          alt="img-one-light"
+          priority
+        />
+        <p className="text-base font-medium md:text-lg">
+          Something went wrong!
+        </p>
+        <Button asChild>
+          <Link href={'/documents'}>Go Back</Link>
+        </Button>
+      </div>
+    </main>
   );
 }
